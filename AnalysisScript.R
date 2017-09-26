2395bf2cd6fd21471cf098ea6219c33a69f6d8e1
lecroc <clecrone@compasspoint-media.com>
2017-09-25 22:07:13 -0500

    1st commit with initial script

---
+++ AnalysisScript.R
@@ -0,0 +1,95 @@
+## Reproducible Research Project 1
+
+# load libraries
+library(dplyr)
+library(ggplot2)
+library(DMwR)
+
+# Get Data
+
+d1<-read.csv(unz("activity.zip", "activity.csv"))
+
+# create a vector of complete cases
+
+good<-complete.cases(d1)
+
+# keep only complete cases
+
+d2<-d1[good,]
+
+# summarize to steps taken by day
+
+d3<-d2 %>%
+  group_by(date) %>%
+  summarize(steps=sum(steps))
+
+# Histogram of steps by day
+
+plot1<-ggplot(data=d3, aes(d3$steps)) + geom_histogram(binwidth = 2500, col="black", fill="blue")
+plot1<-plot1+labs(x="Steps", y="Count of Days", title="Histogram of Steps per Day")
+
+plot1
+
+# mean and median steps by day
+
+stepmean<-mean(d3$steps)
+
+stepmedian<-median(d3$steps)
+
+stepmean
+
+stepmedian
+
+# summarize by avg steps taken by interval
+
+d4<-d2
+
+d4$interval<-as.factor(d4$interval)
+
+d5<-d4 %>%
+  group_by(interval) %>%
+  summarize(steps=mean(steps))
+
+# plot avg steps by interval
+
+plot2<-ggplot(d5, aes(x=interval, y=steps, group=1))+geom_line()
+
+plot2
+
+# find interval with most steps taken
+
+max<-max(d5$steps)
+
+maxint<-subset(d5, steps==max)
+
+
+# Calculate number of rows with NA's
+
+NAnumb<-nrow(d1)-nrow(d2)
+
+NAnumb
+
+# Impute NAs with knn
+
+d6<-knnImputation(d1, k=5)
+
+d7<-d6 %>%
+  group_by(date) %>%
+  summarize(steps=sum(steps))
+
+# Histogram of steps by day
+
+plot3<-ggplot(data=d7, aes(d7$steps)) + geom_histogram(binwidth = 2500, col="black", fill="blue")
+plot3<-plot1+labs(x="Steps", y="Count of Days", title="Histogram of Steps per Day - Imputed N/A")
+
+plot3
+
+# mean and median steps by day
+
+impstepmean<-mean(d7$steps)
+
+impstepmedian<-median(d7$steps)
+
+impstepmean
+
+impstepmedian
